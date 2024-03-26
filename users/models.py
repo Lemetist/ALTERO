e5dd96983ff8028e3f6fde4056788ebbe21a0dce
Lemetist <Lemetist@gmail.com>
2024-03-26 23:27:47 +0300

    add users

---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', blank=True, null=True)
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',
+        blank=True,
+        verbose_name='Groups'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_set',
+        blank=True,
+        verbose_name='User permissions'
+    )
