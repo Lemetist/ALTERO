c3717b51a5daae55739c08a78f13e746546a323c
Lemetist <Lemetist@gmail.com>
2024-04-10 23:29:45 +0300

    update

--- myshop/urls.py
+++ myshop/urls.py
@@ -1,34 +1,17 @@
-"""
-URL configuration for myshop project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.hogme, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
-from shop.views import index, profile, home, contacts
+from django.conf import settings
+from django.conf.urls.static import static
+from shop.views import index, home, contacts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('adv/', include('shop.urls', namespace='products') ),
-    path('login/', include('users.urls', namespace='users')),
-    path('profile/', profile, name='profile'),
-    path('home/',home, name='home'),
-    path('contacts/',contacts, name='contacts')
+    path('adv/', include('shop.urls', namespace='products')),
+    path('login/', include('users.urls')),
+    path('home/', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/models.py
+++ users/models.py
@@ -1,6 +1,29 @@
-from django.contrib.auth.models import AbstractUser
+import uuid
+
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+
 
 class User(AbstractUser):
-    image = models.ImageField(upload_to='users_images', null=True, blank=True)
-    is_verified_email = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='advertisements/%Y/%m/%d', null=True, blank=True)
+
+
+
+
+class Advertisement(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to='advertisements/%Y/%m/%d')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    contact_info = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ['-created']
+
+
